87e79a97bd7f1e9a78891e61221722f56e51771e
artemis-beta <krizar312@gmail.com>
2020-09-26 09:49:19 +0100

    Download test

---
+++ scripts/test.ps1
@@ -0,0 +1,7 @@
+$url = "https://github.com/AlbertBall/railway-dot-exe/releases/download/v2.5.1/Release.v2.5.1.zip"
+
+$output = "RailwayOperationSimulator.zip"
+
+$wc = New-Object System.Net.WebClient
+
+$wc.DownloadFile($url, $output)
