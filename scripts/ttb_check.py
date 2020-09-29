96bc1b03b54ea0a0dd379fd69476d73bc8e29b75
Kristian Zarebski <krizar312@gmail.com>
2020-09-29 23:40:57 +0100

    Replace PowerShell script with Python

---
+++ scripts/ttb_check.py
@@ -0,0 +1,21 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('input_file')
+
+validation_result = parser.parse_args().input_file
+
+with open(validation_result) as f:
+    lines = f.readlines()
+    _exit_code = int(lines[0])
+    _msg = '' if len(lines) == 1 else lines[1]
+
+if _exit_code == 0:
+    print("Timetable Validation Passed Successfully")
+    exit(0)
+else:
+    print("Timetable Validation Failed.")
+    if _msg:
+        print("Validation returned: {}".format(_msg))
+    exit(1)
