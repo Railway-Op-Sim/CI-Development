fce137a7dcbd365bb3aa4e1ef290f5c35438359f
Kristian Zarębski <krizar312@gmail.com>
2020-09-29 22:22:56 +0100

    Update ttb_check.ps1

--- scripts/ttb_check.ps1
+++ scripts/ttb_check.ps1
@@ -2,7 +2,8 @@ $result = cat .\Output.txt
 
 if( "0" -ne $result )
 {
-    Write-Output $result
+    Write-Output "Timetable Validation Failed with Message: "
+	Write-Output $result
     exit 12345
 }
 else
