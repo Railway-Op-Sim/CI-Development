99972eac72273646acae3a5196f0d7afce27796f
artemis-beta <krizar312@gmail.com>
2020-09-29 18:15:18 +0100

    Use Powershell script for ttb validator

---
+++ scripts/ttb_check.ps1
@@ -0,0 +1,17 @@
+Expand-Archive -LiteralPath 'software\TimetableChecker.zip'
+Copy-Item -Path 'test_ttbs\master.ttb' 'test.ttb'
+Copy-Item -Path 'test_rlys\railway.rly' 'railway.rly'
+.\TimetableChecker\TimetableChecker\TimetableChecker.exe
+
+$result = cat ./Output.txt
+
+if( "0" -ne $result )
+{
+    Write-Output $result
+    exit 12345
+}
+else
+{
+    Write-Output "Timetable Validation Successful"
+    exit 0
+}
