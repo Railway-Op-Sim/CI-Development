b60ed5ce2f12ceae57ecee5b15abdf926f89b57a
Kristian Zarebski <krizar312@gmail.com>
2022-01-26 21:32:41 +0000

    Updated metadata file creation

--- scripts/metabuild.py
+++ scripts/metabuild.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
+import datetime
 import toml
+import semver
 import glob
 import sys
 import logging
@@ -67,7 +69,7 @@ if 'ttb_files' not in metadata:
         metadata['ttb_files'] = []
     elif 'ttb_files' not in metadata or not metadata['ttb_files']:
         metadata['ttb_files'] = [os.path.basename(t) for t in ttb_files]
-        logger.info(f"Found timetable files '{metadata['ttb_files']}'")
+        logger.info(f"Found timetable files {metadata['ttb_files']}")
         
 if 'ssn_files' not in metadata:
     ssn_files = glob.glob(os.path.join(proj_dir, 'Sessions', '*.ssn'))
@@ -76,14 +78,87 @@ if 'ssn_files' not in metadata:
         metadata['ssn_files'] = []
     elif 'ssn_files' not in metadata or not metadata['ssn_files']:
         metadata['ssn_files'] = [os.path.basename(s) for s in ssn_files]
-        logger.info("Found session files '{metadata['ssn_files']}'")
-        
+        logger.info(f"Found session files {metadata['ssn_files']}")
+
+if 'graphic_files' not in metadata:
+    graphic_files = glob.glob(os.path.join(proj_dir, 'Graphics', '*'))
+    if not graphic_files:
+        logger.info("No graphic files were found.")
+        metadata['graphic_files'] = []
+    elif 'graphic_files' not in metadata or not metadata['graphic_files']:
+        metadata['graphic_files'] = [os.path.basename(s) for s in graphic_files]
+        logger.info(f"Found graphic files {metadata['graphic_files']}")
+        if not 'minimum_required' in metadata:
+            metadata['minimum_required'] = '2.4.0'
+
+if 'img_files' not in metadata:
+    img_files = glob.glob(os.path.join(proj_dir, 'Images', '*'))
+    if not img_files:
+        logger.info("No image files were found.")
+        metadata['img_files'] = []
+    elif 'img_files' not in metadata or not metadata['img_files']:
+        metadata['img_files'] = [os.path.basename(s) for s in img_files]
+        logger.info(f"Found image files {metadata['img_files']}")
+
+if 'doc_files' not in metadata:
+    doc_files = glob.glob(os.path.join(proj_dir, 'Documentation', '*'))
+    if not doc_files:
+        logger.info("No doc files were found.")
+        metadata['doc_files'] = []
+    elif 'doc_files' not in metadata or not metadata['doc_files']:
+        metadata['doc_files'] = [os.path.basename(s) for s in doc_files]
+        logger.info(f"Found doc files {metadata['doc_files']}")
+
+if 'release_date' not in metadata:
+    release = datetime.datetime.now().strftime("%Y-%m-%d")
+    logger.info("Setting release date to '{release}'")
+    metadata['release_date'] = release
+
+
+def get_version():
+    p = subprocess.Popen(['git', 'describe', '--abbrev=0', '--tags'], cwd=proj_dir,
+                         stdout=subprocess.PIPE, encoding='UTF-8')
+    p.wait()
+    if p.returncode != 0:
+        retrieved = None
+    else:
+        version = p.communicate().strip()
+        retrieved, _ = version
+    
+    version_current = metadata.get("version", None)
+    
+    if version_current:
+        try:
+            semver_current = semver.VersionInfo(version_current)
+        except ValueError:
+            logger.error("Current version tag is not a valid semantic version, setting to 1.0.0")
+            metadata["version"] = "1.0.0"
+            return
+    if retrieved:
+        try:
+            semver_retr = semver.VersionInfo(retrieved)
+        except ValueError:
+            logger.error("Retrieved version tag is not a valid semantic version, using current")
+            metadata["version"] = version_current
+            return
+
+    if version_current and retrieved:
+        if semver_retr > semver_current:
+            metadata["version"] = retrieved
+        else:
+            metadata["version"] = version_current
+        return
+    else:
+        logger.error("No version tag set, using 1.0.0")
+        metadata["version"] = "1.0.0"
+
+get_version()
+
+
 if re.findall(r'^(\w{2})\-', repo_name) and not 'country_code' in metadata:
     metadata['country_code'] = re.findall(r'^(\w{2})\-', repo_name)[0].upper()
     logger.info(f"Found country code '{metadata['country_code']}'")
 
-if 'name' not in metadata:
-    metadata['name'] = ""
 
 if 'description' not in metadata:
     metadata['description'] = ""
@@ -91,6 +166,9 @@ if 'description' not in metadata:
 if 'display_name' not in metadata:
     metadata['display_name'] = repo_name.split('-', 1)[1].replace('-', ' ').title()
     logger.info(f"Set display name '{metadata['display_name']}'")
+
+if 'name' not in metadata:
+    metadata['name'] = metadata['display_name']
         
 p = subprocess.Popen(['git', 'rev-list', '--max-parents=0', 'HEAD'], cwd=proj_dir, stdout=subprocess.PIPE,
                      encoding='UTF-8')
