19f8fa8826e37f5f2b44db3f6d7c4341e0841c9f
Kristian Zarębski <krizar312@gmail.com>
2022-02-06 19:55:21 +0000

    Use latest ROS and fix ttb test

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,4 +1,4 @@
-$url = "https://github.com/AlbertBall/railway-dot-exe/releases/download/v2.5.1/Release.v2.5.1.zip"
+$url = "https://github.com/AlbertBall/railway-dot-exe/releases/download/v2.11.1/Release.v2.11.1.zip"
 
 $output = "RailwayOperationSimulator.zip"
 
@@ -11,5 +11,5 @@ Expand-Archive -LiteralPath $output
 
 Get-ChildItem .\RailwayOperationSimulator
 
-Copy-Item -Path '.\RailwayOperationSimulator\Release v2.5.1' '.\RailwayOperationSimulator\Release_v_2_5_1' -Recurse
-Remove-Item -Recurse -Force '.\RailwayOperationSimulator\Release v2.5.1'
+Copy-Item -Path '.\RailwayOperationSimulator\Release v2.11.1' '.\RailwayOperationSimulator\Release_v_2_11_1' -Recurse
+Remove-Item -Recurse -Force '.\RailwayOperationSimulator\Release v2.11.1'
